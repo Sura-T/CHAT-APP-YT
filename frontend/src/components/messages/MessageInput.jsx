598f8a8fc555bb529652a5d9b3228e018a00964f
sura-t <surafeltakele87@gmail.com>
2024-08-05 12:32:52 +0300

    messages components fixed

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,62 +1,54 @@
 import { useState } from "react";
-import {BsSend} from "react-icons/bs";
+import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
-const [message, setMessage] = useState("");
-const {loading, sendMessage}= useSendMessage();
+	const [message, setMessage] = useState("");
+	const { loading, sendMessage } = useSendMessage();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!message) return;
-    await sendMessage(message);
-    setMessage("");
-  }
-  return (
-    <form className="my-3 px-4" onSubmit={handleSubmit}>
-        <div className="w-full relative">
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!message) return;
+		await sendMessage(message);
+		setMessage("");
+	};
 
-            <input type="text"
-            className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
-            placeholder="Send a message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            />
-
-            <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3"></button>
-
-            {loading ? <div className="loading loading-spinner"></div> : <BsSend />}
-
-        </div>
-      
-    </form>
-  )
-}
-
+	return (
+		<form className='px-4 my-3' onSubmit={handleSubmit}>
+			<div className='w-full relative'>
+				<input
+					type='text'
+					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+					placeholder='Send a message'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+					{loading ? <div className='loading loading-spinner'></div> : <BsSend />}
+				</button>
+			</div>
+		</form>
+	);
+};
 export default MessageInput;
 
-
-// START CODE 
-// import {BsSend} from "react-icons/bs";
+// STARTER CODE SNIPPET
+// import { BsSend } from "react-icons/bs";
 
 // const MessageInput = () => {
-//   return (
-//     <form className="my-3 px-4">
-//         <div className="w-full">
-
-//             <input type="text"
-//             className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
-//             placeholder="Send a message"
-//             />
-
-//             <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3"></button>
-
-//             <BsSend />
-
-//         </div>
-      
-//     </form>
-//   )
-// }
-
+// 	return (
+// 		<form className='px-4 my-3'>
+// 			<div className='w-full'>
+// 				<input
+// 					type='text'
+// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+// 					placeholder='Send a message'
+// 				/>
+// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+// 					<BsSend />
+// 				</button>
+// 			</div>
+// 		</form>
+// 	);
+// };
 // export default MessageInput;
